cdf9e0bc00702d6eefcfaa3e01e4b49ce276c6e2
kavyakammaripalle <46981255+kavyakammaripalle@users.noreply.github.com>
2019-07-17 11:32:53 -0500

    Add files via upload
    variables, data types: Strings, inbuilt functions and f strings

---
+++ Day-1 Session.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#To check version of python
+import sys
+sys.version
+
+
+# In[2]:
+
+
+#To print 
+print("hello world")
+
+
+# In[5]:
+
+
+#Variable declaration
+x=1
+ab2=1
+_abc=2
+print(x+ab2+_abc)
+print(x-ab2+_abc)
+
+
+# In[8]:
+
+
+#Datatypes: String
+v1="kavya"
+v2='kiran'
+v3="Firstname is 'kavya', Lastname is 'Kiran'"
+print(v1)
+print(v2)
+print(v1+v2)
+print(v3)
+
+
+# In[12]:
+
+
+#String built-in functions
+name='kavya kiran'
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+
+# In[22]:
+
+
+#fstrings(creates a placeholder for storing a value and prints on the output)
+s='kavya'
+t=(f"{s} is my name. My full name is {s} kiran.{s} is taking python training.")
+print(t)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
